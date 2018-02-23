bb8072c059f0b18879ea705552f3e8a92dd82ff3
marszalek-m <36745252+marszalek-m@users.noreply.github.com>
2018-02-23 11:08:22 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
-#include "trygonometria.h"
+#ifdef USE_TRIGONOMETRY_DEGREE
+	#include "trygonometria.h"
+#else
+	#include <cmath>
+#endif
 int main() { 
 
-double wynik_sin = degreemath::sin_degree(90.0);
-double wynik_cos = degreemath::cos_degree(0.0);
-double wynik_tg = degreemath::tg_degree(45.0);
-double wynik_ctg = degreemath::ctg_degree(45.0);
+#ifdef USE_TRIGONOMETRY_DEGREE
+	double wynik_sin = degreemath::sin_degree(90.0);
+	double wynik_cos = degreemath::cos_degree(0.0);
+	double wynik_tg = degreemath::tg_degree(45.0);
+	double wynik_ctg = degreemath::ctg_degree(45.0);
+
+#else
+	double wynik_sin = sin(90.0);
+	double wynik_cos = cos(0.0);
+	double wynik_tg = tan(45.0);
+	double wynik_ctg = 1/tan(45.0);
+#endif
 return 0; }
